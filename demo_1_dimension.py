af132b0518e844654942806768b5b75a9d234890
lucas schott <lucas.schott97@gmail.com>
2019-01-27 19:12:40 +0100

    rendering 2d

--- demo_1_dimension.py
+++ demo_1_dimension.py
@@ -1,11 +1,12 @@
 import gym
 import gym_multi_dimensional
 env = gym.make('MultiDimensional-v0')
+env.init(1)
 
 for i_episode in range(10):
     observation = env.reset()
     cum_reward=0
-    for t in range(200):
+    for t in range(100):
         env.render()
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
--- gym-multi-dimensional/gym_multi_dimensional/envs/multi_dimensional_env.py
+++ gym-multi-dimensional/gym_multi_dimensional/envs/multi_dimensional_env.py
@@ -7,40 +7,53 @@ from gym.utils import seeding
 class MultiDimensionalEnv(gym.Env):
     """
     Description:
-        An agent is in a square. The goal is to find the green door by
-        increasing and reducing his velocity in the two directions.
+        An agent is in a n-cube (n dimension hypercube). The goal is to reach
+        the green (n-1)-face of the n-cube (the green vertex of the segment if
+        n=1, the green edge of the square if n=2, the green face of the cube if
+        n=3, ...) by increasing and reducing his velocity in the n directions.
 
     Observation: 
-        Type: Box(4)
-        Num Observation     Min     Max
-        0   Agent X Position  -1      1
-        1   Agent Y Position  -1      1
-        2   Agent X Velocity  -0.1    0.1
-        3   Agent Y Velocity  -0.1    0.1
+        Type: Box(2n)
+        Num     Observation                         Min     Max
+        0       Agent 1st direction position        -1      1
+        1       Agent 2nd direction position        -1      1
+        ...     ...                                 ...     ...
+        n-1     Agent nth direction position        -1      1
+        n       Agent 1st direction velocity        -0.1    0.1
+        ...     ...                                 ...     ...
+        2n-2    Agent (n-1)th direction velocity    -0.1    0.1
+        2n-1    Agent nth direction velocity        -0.1    0.1
 
     Actions:
-        Type: Discrete(5)
-        Num Action
-        0   Don't Accelerate agent
-        1   Accelerate agent to -1 orientation in X direction
-        2   Accelerate agent to 1 orientation in X direction
-        3   Accelerate agent to -1 orientation in Y direction
-        4   Accelerate agent to 1 orientation in Y direction
+        Type: Discrete(2n+1)
+        Num     Action
+        0       Don't Accelerate the agent
+        1       Accelerate the agent to -1 orientation in the 1st direction
+        2       Accelerate the agent to 1 orientation in the 1st direction
+        3       Accelerate the agent to -1 orientation in the 2nd direction
+        4       Accelerate the agent to 1 orientation in the 2nd  direction
+        ...     ...
+        2n-1    Accelerate the agent to -1 orientation in the nth direction
+        2n      Accelerate the agent to 1 orientation in the nth direction
 
         
     Reward:
-        Reward is 1 if the agent find the green door
-        Raward is 0.1 if the agent find the red door
+        Reward is 1 if the agent find the green (n-1)-face
+        Raward is 0.1 if the agent find a red (n-1)-face
 
     Starting State
-        Agent X position is assigned a uniform random value between -0.6 and-0.4
-        Agent Y position is 0
-        Agent X velocity is assigned to 0
-        Agent Y velocity is assigned to 0
+        Agent 1st direction position is assigned a uniform random value between -0.6 and-0.4
+        Agent 2nd direction position is assigned to 0
+        ...
+        Agent nth direction position is assigned to 0
+        Agent 1st direction velocity is assigned to 0
+        Agent 2nd direction velocity is assigned to 0
+        ...
+        Agent nth direction velocity is assigned to 0
 
     Episode Termination:
-        Agent find any door
-        Episode length is greater than 100
+        Agent find any (n-1)-face
+        Episode length is greater than 100n
     """
 
     metadata = {
@@ -50,14 +63,21 @@ class MultiDimensionalEnv(gym.Env):
 
 
     def __init__(self):
+        pass
+
+    def init(self,n):
+        self.n = n
+
         self.max_position = 1
         self.max_speed = 0.1
         
-        self.high = np.array([self.max_position,self.max_position,
-            self.max_speed,self.max_speed])
+        self.high = np.array([
+            np.ones((self.n))*self.max_position,
+            np.ones((self.n))*self.max_speed
+            ])
         self.low = -self.high
 
-        self.action_space = spaces.Discrete(5)
+        self.action_space = spaces.Discrete(2*self.n+1)
         self.observation_space = spaces.Box(
                 self.low, self.high, dtype=np.float32)
 
@@ -94,20 +114,20 @@ class MultiDimensionalEnv(gym.Env):
         velocity = self.state[1]
         if direction > -1:
             velocity[direction] += (orientation)*0.01
-            for direction in range(len(velocity)):
+            for direction in range(self.n):
                 if velocity[direction]>0:
                     velocity[direction] = max(0,velocity[direction]-0.001)
                 else:
                     velocity[direction] = min(0,velocity[direction]+0.001)
             velocity[direction] = np.clip(velocity[direction], -self.max_speed, self.max_speed)
             position += velocity
-            for direction in range(len(position)):
+            for direction in range(self.n):
                 position[direction] = np.clip(position[direction], -self.max_position, self.max_position)
 
         #door
         green_door = bool(position[0] >= self.max_position)
         red_door = bool(position[0] <= -self.max_position)
-        for direction in range(1,len(position)):
+        for direction in range(1,self.n):
             red_door = red_door or bool(position[direction] <= -self.max_position)
             red_door = red_door or bool(position[direction] >= self.max_position)
         done = bool(green_door or red_door)
@@ -127,93 +147,160 @@ class MultiDimensionalEnv(gym.Env):
 
     def reset(self):
         self.state = np.array([
-            [self.np_random.uniform(low=-0.6, high=-0.4), 0],
-            [0                                          , 0]])
+            np.zeros((self.n)),
+            np.zeros((self.n))
+            ])
+        self.state[0,0] = self.np_random.uniform(low=-0.6, high=-0.4)
         return np.array(self.state)
 
 
     def render(self, mode='human'):
-        screen_width = 600
-        screen_height = 600
 
-        world_width = self.max_position*2
-        scale = screen_width/world_width
+        if self.n==1:
+            screen_width = 600
+            screen_height = 200
 
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_width, screen_height)
+            world_width = self.max_position*2
+            scale = screen_width/world_width
 
-            #reddoor
-            reddoorx1 = 0
-            reddoorx2 = screen_width
-            reddoory1 = screen_height
-            reddoory2 = screen_height-10
-            self.reddoor = rendering.FilledPolygon(
-                    [(reddoorx1, reddoory1),
-                        (reddoorx2, reddoory1),
-                        (reddoorx2, reddoory2),
-                        (reddoorx1, reddoory2)]
-                    )
-            self.reddoor.set_color(1,0,0)
-            self.viewer.add_geom(self.reddoor)
-            
-            reddoorx1 = 0
-            reddoorx2 = 10
-            reddoory1 = screen_height
-            reddoory2 = 0
-            self.reddoor = rendering.FilledPolygon(
-                    [(reddoorx1, reddoory1),
-                        (reddoorx2, reddoory1),
-                        (reddoorx2, reddoory2),
-                        (reddoorx1, reddoory2)]
-                    )
-            self.reddoor.set_color(1,0,0)
-            self.viewer.add_geom(self.reddoor)
-            
-            reddoorx1 = 0
-            reddoorx2 = screen_width
-            reddoory1 = 0
-            reddoory2 = 10
-            self.reddoor = rendering.FilledPolygon(
-                    [(reddoorx1, reddoory1),
-                        (reddoorx2, reddoory1),
-                        (reddoorx2, reddoory2),
-                        (reddoorx1, reddoory2)]
-                    )
-            self.reddoor.set_color(1,0,0)
-            self.viewer.add_geom(self.reddoor)
-            
-            #greendoor
-            greendoorx1 = screen_width-10
-            greendoorx2 = screen_width
-            greendoory1 = 0
-            greendoory2 = screen_height
-            self.greendoor = rendering.FilledPolygon(
-                    [(greendoorx1, greendoory1),
-                        (greendoorx2, greendoory1),
-                        (greendoorx2, greendoory2),
-                        (greendoorx1, greendoory2)]
-                    )
-            self.greendoor.set_color(0,1,0)
-            self.viewer.add_geom(self.greendoor)
-            
-            #agent
-            agentwidth = 20
-            agentheight = 20
-            l,r,t,b = (-agentwidth/2, agentwidth/2, agentheight/2,
-                    -agentheight/2)
-            self.agent = rendering.FilledPolygon([(l,b),(l,t),(r,t),(r,b)])
-            self.agenttrans = rendering.Transform()
-            self.agent.add_attr(self.agenttrans)
-            self.viewer.add_geom(self.agent)
+            agenty = 100
 
+            if self.viewer is None:
+                from gym.envs.classic_control import rendering
+                self.viewer = rendering.Viewer(screen_width, screen_height)
 
+                #track
+                tracky = 100
+                self.track = rendering.Line((0,tracky),(screen_width,tracky))
+                self.track.set_color(0,0,0)
+                self.viewer.add_geom(self.track)
 
-        if self.state is None: return None
+                #agent
+                agentwidth = 20
+                agentheight = 20
+                agenty = tracky
+                l,r,t,b = (-agentwidth/2, agentwidth/2, agentheight/2,
+                        -agentheight/2)
+                self.agent = rendering.FilledPolygon([(l,b),(l,t),(r,t),(r,b)])
+                self.agenttrans = rendering.Transform()
+                self.agent.add_attr(self.agenttrans)
+                self.viewer.add_geom(self.agent)
 
-        pos = self.state[0]
-        agent_position = (pos+self.max_position)*scale
-        self.agenttrans.set_translation(agent_position[0],agent_position[1])
+                #greenflag
+                greenflagx1 = screen_width-10
+                greenflagx2 = screen_width
+                greenflagy1 = tracky+20
+                greenflagy2 = tracky-20
+                self.greenflag = rendering.FilledPolygon([
+                    (greenflagx1, greenflagy1),
+                    (greenflagx2, greenflagy1),
+                    (greenflagx2, greenflagy2),
+                    (greenflagx1, greenflagy2)
+                    ])
+                self.greenflag.set_color(0,1,0)
+                self.viewer.add_geom(self.greenflag)
+
+                #redflag
+                redflagx1 = 0
+                redflagx2 = 10
+                redflagy1 = tracky+20
+                redflagy2 = tracky-20
+                self.redflag = rendering.FilledPolygon([
+                    (redflagx1, redflagy1),
+                    (redflagx2, redflagy1),
+                    (redflagx2, redflagy2),
+                    (redflagx1, redflagy2)
+                    ])
+                self.redflag.set_color(1,0,0)
+                self.viewer.add_geom(self.redflag)
+                
+                if self.state is None: return None
+
+                pos = self.state[0]
+                agentx = (pos+self.max_position)*scale
+                self.agenttrans.set_translation(agentx,agenty)
+
+        elif self.n==2:
+            screen_width = 600
+            screen_height = 600
+
+            world_width = self.max_position*2
+            scale = screen_width/world_width
+
+            if self.viewer is None:
+                from gym.envs.classic_control import rendering
+                self.viewer = rendering.Viewer(screen_width, screen_height)
+
+                #reddoor
+                reddoorx1 = 0
+                reddoorx2 = screen_width
+                reddoory1 = screen_height
+                reddoory2 = screen_height-10
+                self.reddoor = rendering.FilledPolygon([
+                    (reddoorx1, reddoory1),
+                    (reddoorx2, reddoory1),
+                    (reddoorx2, reddoory2),
+                    (reddoorx1, reddoory2)
+                    ])
+                self.reddoor.set_color(1,0,0)
+                self.viewer.add_geom(self.reddoor)
+                
+                reddoorx1 = 0
+                reddoorx2 = 10
+                reddoory1 = screen_height
+                reddoory2 = 0
+                self.reddoor = rendering.FilledPolygon([
+                    (reddoorx1, reddoory1),
+                    (reddoorx2, reddoory1),
+                    (reddoorx2, reddoory2),
+                    (reddoorx1, reddoory2)
+                    ])
+                self.reddoor.set_color(1,0,0)
+                self.viewer.add_geom(self.reddoor)
+                
+                reddoorx1 = 0
+                reddoorx2 = screen_width
+                reddoory1 = 0
+                reddoory2 = 10
+                self.reddoor = rendering.FilledPolygon([
+                    (reddoorx1, reddoory1),
+                    (reddoorx2, reddoory1),
+                    (reddoorx2, reddoory2),
+                    (reddoorx1, reddoory2)
+                    ])
+                self.reddoor.set_color(1,0,0)
+                self.viewer.add_geom(self.reddoor)
+                
+                #greendoor
+                greendoorx1 = screen_width-10
+                greendoorx2 = screen_width
+                greendoory1 = 0
+                greendoory2 = screen_height
+                self.greendoor = rendering.FilledPolygon([
+                    (greendoorx1, greendoory1),
+                    (greendoorx2, greendoory1),
+                    (greendoorx2, greendoory2),
+                    (greendoorx1, greendoory2)
+                    ])
+                self.greendoor.set_color(0,1,0)
+                self.viewer.add_geom(self.greendoor)
+                
+                #agent
+                agentwidth = 20
+                agentheight = 20
+                l,r,t,b = (-agentwidth/2, agentwidth/2, agentheight/2,
+                        -agentheight/2)
+                self.agent = rendering.FilledPolygon([(l,b),(l,t),(r,t),(r,b)])
+                self.agenttrans = rendering.Transform()
+                self.agent.add_attr(self.agenttrans)
+                
+                self.viewer.add_geom(self.agent)
+                
+                if self.state is None: return None
+
+                pos = self.state[0]
+                agent_position = (pos+self.max_position)*scale
+                self.agenttrans.set_translation(agent_position[0],agent_position[1])
 
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
