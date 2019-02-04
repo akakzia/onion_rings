72da703f61265a60551633cd3124bcad782159f2
Hector Roussille <hector.roussille@gmail.com>
2019-02-04 20:40:13 +0100

    Added replay buffer visualization / fixed reference bug making replay buffer vizualization appear as a circular path

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,5 +1,8 @@
 import collections
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import copy
 
 class ReplayBuffer():
 
@@ -12,6 +15,9 @@ class ReplayBuffer():
         """ If maximum buffer size reached , remove old experience """
         if len(self.buffer) == self.buffer_size:
             self.buffer.popleft()
+
+        state = copy.deepcopy(state)
+        new_state = copy.deepcopy(new_state)
 
         """ Add new experience """
         self.buffer.append((state, action, reward, new_state))
@@ -29,3 +35,35 @@ class ReplayBuffer():
 
     def clear(self):
         self.buffer.clear()
+
+    def visualize(self):
+
+        pos_init = [exp[0][0] for exp in self.buffer]
+        pos_final = [exp[3][0] for exp in self.buffer]
+
+        red = 1.0
+        green = 0
+        blue = 0
+
+        step = 1.0 / (len(self.buffer) / 2)
+
+        plt.set_cmap('RdYlGn')
+
+        for i in range(len(pos_init)):
+            x_pos = pos_init[i][0]
+            y_pos = pos_init[i][1]
+            x_vel = pos_final[i][0] - x_pos
+            y_vel = pos_final[i][1] - y_pos
+
+            color = matplotlib.colors.to_hex((red, green, blue))
+            plt.plot(x_pos, y_pos, '.', color=color)
+            plt.arrow(x_pos, y_pos, x_vel, y_vel, color="blue", width=0.001, head_width=0.008)
+
+            if green < 1.0:
+                green = min(green + step, 1.0)
+            else:
+                red = max(red - step, 0)
+
+
+        plt.show()
+
