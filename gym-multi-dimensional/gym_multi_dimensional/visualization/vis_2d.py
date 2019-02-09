1c1a4b4eb5da5667ce1345a9727495fd9ab5424f
Lucas Schott <lucas.schott97@gmail.com>
2019-02-09 15:36:34 +0000

    Q values arrow 2D visualisation

--- gym-multi-dimensional/gym_multi_dimensional/visualization/vis_2d.py
+++ gym-multi-dimensional/gym_multi_dimensional/visualization/vis_2d.py
@@ -1,7 +1,8 @@
+import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-def visualize(rb):
+def visualize_RB(rb):
 
     red = 1.0
     green = 0
@@ -26,6 +27,34 @@ def visualize(rb):
         else:
             red = max(red - step, 0)
 
+
+    ax.add_patch(plt.Rectangle((-1, -1), 2, 2, angle=0.0, facecolor='none', edgecolor='blue', linewidth=1))
+    plt.show()
+
+
+def visualize_Q(q_values):
+
+    qs = q_values[:,0]
+    qs = qs + np.max(qs)
+    qs = qs - np.min(qs)
+    qs = qs/np.max(qs) #normalisation
+    states = q_values[:,1:]
+
+    red = 1.0
+    green = 1.0
+    blue = 0
+
+    fig, ax = plt.subplots(1)
+    plt.set_cmap('RdYlGn')
+
+    for q,state in zip(qs,states):
+
+        x_pos , y_pos = state[0],state[1]
+        x_vel , y_vel = state[2]*10,state[3]*10
+
+        color = matplotlib.colors.to_hex((red*(1-q), green*q, blue))
+        plt.plot(x_pos, y_pos, '.', color=color)
+        plt.arrow(x_pos, y_pos, x_vel, y_vel, color=color, width=0.001, head_width=0.008)
 
     ax.add_patch(plt.Rectangle((-1, -1), 2, 2, angle=0.0, facecolor='none', edgecolor='blue', linewidth=1))
     plt.show()
