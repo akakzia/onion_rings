9e3070e58e0879af3597ee35bc11259aed543595
Hector Roussille <hector.roussille@gmail.com>
2019-02-21 16:07:48 +0100

    plot saving / proper folder structure

--- gym-multi-dimensional/gym_multi_dimensional/visualization/vis_2d.py
+++ gym-multi-dimensional/gym_multi_dimensional/visualization/vis_2d.py
@@ -3,7 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-def visualize_RB(rb, acceleration=False):
+def visualize_RB(rb, acceleration=False, save=False, path=''):
 
     red = 1.0
     green = 0
@@ -37,10 +37,15 @@ def visualize_RB(rb, acceleration=False):
 
 
     ax.add_patch(plt.Rectangle((-1, -1), 2, 2, angle=0.0, facecolor='none', edgecolor='blue', linewidth=1))
+
+    if save:
+        plt.savefig(path + "/rb.png")
+
     plt.show()
 
 
-def visualize_Q_arrow(q_values):
+
+def visualize_Q_arrow(q_values, save=False, path=''):
 
     qs = q_values[:,0]
     qs -= np.min(qs)
@@ -64,10 +69,15 @@ def visualize_Q_arrow(q_values):
         plt.arrow(x_pos, y_pos, x_vel, y_vel, color=color, width=0.001, head_width=0.008)
 
     ax.add_patch(plt.Rectangle((-1, -1), 2, 2, angle=0.0, facecolor='none', edgecolor='blue', linewidth=1))
+
+    if save:
+        plt.savefig(path + "/Q_arrow.png")
+
     plt.show()
 
 
-def visualize_Q_contour(q_values):
+
+def visualize_Q_contour(q_values, save=False, path=''):
 
     qs = q_values[:,0]
     states = q_values[:,1:]
@@ -78,9 +88,14 @@ def visualize_Q_contour(q_values):
     plt.tricontourf(states[:,0], states[:,1], qs)
 
     ax.add_patch(plt.Rectangle((-1, -1), 2, 2, angle=0.0, facecolor='none', edgecolor='blue', linewidth=1))
+
+    if save:
+        plt.savefig(path + '/Q_contour.png')
+
     plt.show()
 
-def visualize_Q_contour_time(all_q_values, path):
+
+def visualize_Q_contour_time(all_q_values, save=False, path=''):
 
     fig = plt.figure()
 
@@ -94,7 +109,9 @@ def visualize_Q_contour_time(all_q_values, path):
 
     anim = animation.FuncAnimation(fig, animate, interval=200, frames=len(all_q_values))
 
-    anim.save(path + ".gif", writer='imagemagick', fps=4)
+    if save:
+        anim.save(path + "/Q_contour_time.gif", writer='imagemagick', fps=4)
+
 
 
 
