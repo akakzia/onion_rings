9553475202c5ea7fb009793c3400eb4904c76e6b
Lucas Schott <lucas.schott97@gmail.com>
2019-02-24 00:25:37 +0100

    changed environment register name

--- gym-multi-dimensional/gym_multi_dimensional/__init__.py
+++ gym-multi-dimensional/gym_multi_dimensional/__init__.py
@@ -1,12 +1,12 @@
 from gym.envs.registration import register
 from gym_multi_dimensional.envs import multi_dimensional_env as md
 
-def dynamic_register(n_dimensions=1, env_description=md.MultiDimensionalEnv.default_description, continuous=True,
+def dynamic_register(n_dimensions=2, env_description=md.MultiDimensionalEnv.default_description, continuous=True,
         acceleration=True):
-    register(id="MultiDimensional-v{}".format(n_dimensions),
+    register(id="MultiDimensional-v0",
              entry_point="gym_multi_dimensional.envs:MultiDimensionalEnv",
              kwargs={"n_dimensions":n_dimensions,
                  "env_description":env_description,
                  "continuous":continuous,
                  "acceleration":acceleration})
-    return "MultiDimensional-v{}".format(n_dimensions)
+    return "MultiDimensional-v0"
