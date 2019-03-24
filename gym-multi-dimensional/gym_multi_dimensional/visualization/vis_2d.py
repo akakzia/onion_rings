013e742fab0bc8a45e50d947d62dc2ee783a30df
Lucas Schott <lucas.schott97@gmail.com>
2019-03-24 12:03:22 +0000

    fix pi arrow visualization

--- gym-multi-dimensional/gym_multi_dimensional/visualization/vis_2d.py
+++ gym-multi-dimensional/gym_multi_dimensional/visualization/vis_2d.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
 def visualize_RB(rb, acceleration=False, save=False, path=''):
 
     red = 1.0
@@ -44,32 +45,24 @@ def visualize_RB(rb, acceleration=False, save=False, path=''):
 
 
 
-def visualize_Q_arrow(q_values, save=False, path='', inline=True):
-    qs = q_values[:,0]
-    qs -= np.min(qs)
-    qs /= np.max(qs)
-    states = q_values[:,1:]
-
-    red = 1.0
-    green = 1.0
-    blue = 0
+def visualize_Pi(pi_values, save=False, path='', inline=True):
+    states = pi_values[:,:2]
+    pis = pi_values[:,2:]
 
     fig, ax = plt.subplots(1)
-    plt.set_cmap('RdYlGn')
 
-    for q,state in zip(qs,states):
+    for pi,state in zip(pis,states):
 
         x_pos , y_pos = state[0],state[1]
-        x_vel , y_vel = state[2]*0.1,state[3]*0.1
+        x_pi , y_pi = pi[0]*0.1,pi[1]*0.1
 
-        color = matplotlib.colors.to_hex((red*(1-q), green*q, blue))
-        plt.plot(x_pos, y_pos, '.', color=color)
-        plt.arrow(x_pos, y_pos, x_vel, y_vel, color=color, width=0.001, head_width=0.008)
+        plt.plot(x_pos, y_pos, '.', color="black")
+        plt.arrow(x_pos, y_pos, x_pi, y_pi, color="black", width=0.001, head_width=0.008)
 
-    ax.add_patch(plt.Rectangle((-1, -1), 2, 2, angle=0.0, facecolor='none', edgecolor='blue', linewidth=1))
+    plt.title(r'$Pi(s)$')
 
     if save:
-        plt.savefig(path + "/Q_arrow.png")
+        plt.savefig(path + "/Pi_arrow.png")
     
     if inline is True:
         plt.show()
@@ -77,7 +70,41 @@ def visualize_Q_arrow(q_values, save=False, path='', inline=True):
     else:
         return fig
 
-def visualize_Q_contour(q_values, save=False, path='', inline=True):
+
+def visualize_Pi_time(all_pi_values, save=False, path=''):
+
+    fig, ax = plt.subplots(1)
+
+    def animate(i):
+        fig.clear()
+        ax = fig.add_subplot(111)
+        pi_values = all_pi_values[i]
+
+        states = pi_values[:,:2]
+        pis = pi_values[:,2:]
+
+        for pi,state in zip(pis,states):
+
+            x_pos , y_pos = state[0],state[1]
+            x_pi , y_pi = pi[0]*0.1,pi[1]*0.1
+
+            ax.plot(x_pos, y_pos, '.', color="black")
+            ax.arrow(x_pos, y_pos, x_pi, y_pi, color="black", width=0.001, head_width=0.008)
+
+        ax.set_title(r'$Pi(s)$ timestep : {}'.format(i))
+        ax.set_xlabel('x dimension')
+        ax.set_ylabel('y dimension')
+        ax.set_xticks(np.arange(-1, 1, step=0.1))
+        ax.set_yticks(np.arange(-1, 1, step=0.1))
+
+    anim = animation.FuncAnimation(fig, animate, interval=200, frames=len(all_pi_values))
+
+    if save:
+        anim.save(path + "/Pi_arrow_time.gif", writer='imagemagick', fps=4)
+
+
+
+def visualize_Q(q_values, save=False, path='', inline=True):
 
     qs = q_values[:,0]
     states = q_values[:,1:]
@@ -104,7 +131,8 @@ def visualize_Q_contour(q_values, save=False, path='', inline=True):
     else:
         return fig
 
-def visualize_Q_contour_time(all_q_values, save=False, path=''):
+
+def visualize_Q_time(all_q_values, save=False, path=''):
 
     fig, ax = plt.subplots(1)
 
@@ -130,50 +158,3 @@ def visualize_Q_contour_time(all_q_values, save=False, path=''):
 
     if save:
         anim.save(path + "/Q_contour_time.gif", writer='imagemagick', fps=4)
-
-
-def visualize_Q_arrow_time(all_q_values, save=False, path=''):
-
-    fig, ax = plt.subplots(1)
-
-    plt.set_cmap('RdYlGn')
-
-    def animate(i):
-        fig.clear()
-        ax = fig.add_subplot(111)
-        q_values = all_q_values[i]
-
-        qs = q_values[:,0]
-        qs -= np.min(qs)
-        qs /= np.max(qs)
-        states = q_values[:,1:]
-
-        red = 1.0
-        green = 1.0
-        blue = 0
-
-        for q,state in zip(qs,states):
-
-            x_pos , y_pos = state[0],state[1]
-            x_vel , y_vel = state[2]*0.1,state[3]*0.1
-
-            color = matplotlib.colors.to_hex((red*(1-q), green*q, blue))
-            ax.plot(x_pos, y_pos, '.', color=color)
-            ax.arrow(x_pos, y_pos, x_vel, y_vel, color=color, width=0.001, head_width=0.008)
-
-        ax.add_patch(plt.Rectangle((-1, -1), 2, 2, angle=0.0, facecolor='none', edgecolor='blue', linewidth=1))
-        ax.set_title(r'$Q(s, \pi(s))$ timestep : {}'.format(i))
-        ax.set_xlabel('x dimension')
-        ax.set_ylabel('y dimension')
-        ax.set_xticks(np.arange(-1, 1, step=0.1))
-        ax.set_yticks(np.arange(-1, 1, step=0.1))
-
-    anim = animation.FuncAnimation(fig, animate, interval=200, frames=len(all_q_values))
-
-    if save:
-        anim.save(path + "/Q_arrow_time.gif", writer='imagemagick', fps=4)
-
-
-
-
-
