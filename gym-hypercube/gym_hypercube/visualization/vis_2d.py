c89f6a42f417b290e394cde1586cb91dd9079ddd
Lucas Schott <lucas.schott97@gmail.com>
2019-04-17 22:35:00 +0200

    improved plot readability

--- gym-hypercube/gym_hypercube/visualization/vis_2d.py
+++ gym-hypercube/gym_hypercube/visualization/vis_2d.py
@@ -30,26 +30,22 @@ def visualize_RB(rb, acceleration=False, filter=None, save=False, path=''):
 
         color = matplotlib.colors.to_hex((red, green, blue))
         plt.plot(x_pos, y_pos, '.', color=color)
-        #plt.arrow(x_pos, y_pos, x_vel, y_vel, color="blue", width=0.001, head_width=0.008)
 
         if green < 1.0:
             green = min(green + step, 1.0)
         else:
             red = max(red - step, 0)
 
-    plt.title("Replay buffer states")
-    plt.xlabel('1st dimension')
-    plt.ylabel('2nd dimension')
-    plt.xticks(np.arange(-1, 1, step=0.2))
-    plt.yticks(np.arange(-1, 1, step=0.2))
+    plt.title("Replay buffer states",fontsize=12)
+    plt.xlabel('1st dimension',fontsize=12)
+    plt.ylabel('2nd dimension',fontsize=12)
+    plt.xticks([-1,-0.5,0,0.5,1],fontsize=12)
+    plt.yticks([-1,-0.5,0,0.5,1],fontsize=12)
+    plt.xlim(-1, 1)
+    plt.ylim(-1, 1)
 
     if filter is not None:
         ax.add_artist(plt.Circle(filter.center, filter.size, alpha=0.7, color='r'))
-
-    ax.set_clip_box(matplotlib.transforms.Bbox([[-1, -1], [1, 1]]))
-
-    ax.set_xlim(-1, 1)
-    ax.set_ylim(-1, 1)
 
     cax = divider.append_axes('right', size='5%', pad=0.05)
     norm = matplotlib.colors.Normalize(vmin=0, vmax=1)
@@ -57,12 +53,9 @@ def visualize_RB(rb, acceleration=False, filter=None, save=False, path=''):
     colorbar.set_ticks([0, 1])
     colorbar.ax.set_yticklabels(['Old', 'New'])
 
-
     if save:
         plt.savefig(path + "/rb.png")
-
     plt.show()
-
 
 
 def visualize_Pi(pi_values, save=False, name="Pi_arrow.png", title=r'$\pi(s)$', path='', inline=True):
@@ -79,11 +72,13 @@ def visualize_Pi(pi_values, save=False, name="Pi_arrow.png", title=r'$\pi(s)$', 
         plt.plot(x_pos, y_pos, '.', color="black")
         plt.arrow(x_pos, y_pos, x_pi, y_pi, color="black", width=0.001, head_width=0.02)
 
-    plt.title(title)
-    plt.xlabel('1st dimension')
-    plt.ylabel('2nd dimension')
-    plt.xticks(np.arange(-1, 1, step=0.2))
-    plt.yticks(np.arange(-1, 1, step=0.2))
+    plt.title(title,fontsize=12)
+    plt.xlabel('1st dimension',fontsize=12)
+    plt.ylabel('2nd dimension',fontsize=12)
+    plt.xticks([-1,-0.5,0,0.5,1],fontsize=12)
+    plt.yticks([-1,-0.5,0,0.5,1],fontsize=12)
+    plt.xlim(-1, 1)
+    plt.ylim(-1, 1)
 
     if save:
         plt.savefig(path + "/"+name)
@@ -115,11 +110,13 @@ def visualize_Pi_time(all_pi_values, save=False, name="Pi_arrow_time.gif", title
             ax.plot(x_pos, y_pos, '.', color="black")
             ax.arrow(x_pos, y_pos, x_pi, y_pi, color="black", width=0.001, head_width=0.02)
 
-        ax.set_title(title + r' ; {}'.format(steps_name) + ' {}'.format(int(steps[i])))
-        ax.set_xlabel('1st dimension')
-        ax.set_ylabel('2nd dimension')
-        ax.set_xticks(np.arange(-1, 1, step=0.2))
-        ax.set_yticks(np.arange(-1, 1, step=0.2))
+        plt.title(title + r' ; {}'.format(steps_name) + ' {}'.format(int(steps[i])),fontsize=12)
+        plt.xlabel('1st dimension',fontsize=12)
+        plt.ylabel('2nd dimension',fontsize=12)
+        plt.xticks([-1,-0.5,0,0.5,1],fontsize=12)
+        plt.yticks([-1,-0.5,0,0.5,1],fontsize=12)
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
 
     anim = animation.FuncAnimation(fig, animate, interval=200, frames=len(steps))
 
@@ -137,14 +134,18 @@ def visualize_Q(q_values, save=False, name='Q_contour.png', title=r'$Q(s,\pi(s))
     plt.set_cmap('RdYlGn')
 
     colorset = ax.tricontourf(states[:,0], states[:,1], qs)
-    colorbar = fig.colorbar(colorset)
-    colorbar.ax.set_ylabel('Q values')
+    colorbar = plt.colorbar(colorset, aspect=20, format="%.1e")
+    colorbar.ax.tick_params(labelsize=12)
 
-    ax.set_title(title)
-    ax.set_xlabel('1st dimension')
-    ax.set_ylabel('2nd dimension')
-    ax.set_xticks(np.arange(-1, 1, step=0.2))
-    ax.set_yticks(np.arange(-1, 1, step=0.2))
+    plt.title(title,fontsize=12)
+    plt.xlabel('1st dimension',fontsize=12)
+    plt.ylabel('2nd dimension',fontsize=12)
+    plt.xticks([-1,-0.5,0,0.5,1],fontsize=12)
+    plt.yticks([-1,-0.5,0,0.5,1],fontsize=12)
+    plt.xlim(-1, 1)
+    plt.ylim(-1, 1)
+
+
 
     if save:
         fig.savefig(path + '/'+name)
@@ -169,15 +170,17 @@ def visualize_Q_time(all_q_values, save=False, name="Q_contour_time.gif", title=
         qs = q_values[:,0]
         states = q_values[:,1:]
         colorset = ax.tricontourf(states[:,0], states[:,1], qs)
-        colorbar = plt.colorbar(colorset, aspect=20, format="%.4f")
-        colorbar.ax.set_ylabel('Q values')
-        colorbar.ax.tick_params(labelsize=10)
+        colorbar = plt.colorbar(colorset, aspect=20, format="%.1e")
+        colorbar.ax.tick_params(labelsize=12)
 
-        ax.set_title(title + r' ; {}'.format(steps_name) + ' {}'.format(int(steps[i])))
-        ax.set_xlabel('1st dimension')
-        ax.set_ylabel('2nd dimension')
-        ax.set_xticks(np.arange(-1, 1, step=0.2))
-        ax.set_yticks(np.arange(-1, 1, step=0.2))
+        plt.title(title + r' ; {}'.format(steps_name) + ' {}'.format(int(steps[i])), fontsize=12)
+        plt.xlabel('1st dimension',fontsize=12)
+        plt.ylabel('2nd dimension',fontsize=12)
+        plt.xticks([-1,-0.5,0,0.5,1],fontsize=12)
+        plt.yticks([-1,-0.5,0,0.5,1],fontsize=12)
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
+
 
     anim = animation.FuncAnimation(fig, animate, interval=200, frames=len(steps))
 
